7ec3743c3df671b6f7590e81ea42c7844302998b
mattfreire <freire1996@gmail.com>
2018-04-03 23:43:17 +0200

    Finance Plotly and Ajax Scripts

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,13 +3,17 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from finance.views import company_article_list
+
 from news.views import scrape, news_list
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^notes/', include('notepad.urls', namespace='notes')),
     url(r'^scrape/', scrape, name='scrape'),
-    url(r'^home/', news_list, name='home')
+    url(r'^home/', news_list, name='home'),
+    url(r'^companies/', company_article_list, name='companies')
     
 ]
 

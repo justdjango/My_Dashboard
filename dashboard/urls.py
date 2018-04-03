26bb12796169f79296c95b34c89c3f36ab92a3b9
mattfreire <freire1996@gmail.com>
2018-04-04 00:03:40 +0200

    Finance API View

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from finance.views import company_article_list
+from finance.views import company_article_list, ChartData
 
 from news.views import scrape, news_list
 
@@ -13,7 +13,8 @@ urlpatterns = [
     url(r'^notes/', include('notepad.urls', namespace='notes')),
     url(r'^scrape/', scrape, name='scrape'),
     url(r'^home/', news_list, name='home'),
-    url(r'^companies/', company_article_list, name='companies')
+    url(r'^companies/', company_article_list, name='companies'),
+    url(r'^api/chart/data/$', ChartData.as_view(), name='api-chart-data')
     
 ]
 
--- finance/views.py
+++ finance/views.py
@@ -2,5 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Company
+
 def company_article_list(request):
 	return render(request, "finance/plotly.html", {})
+
+
+
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+    
+    def get(self, request, format=None):
+        articles = dict()
+        for company in Company.objects.all():
+            if company.articles > 0:
+                articles[company.name] = company.articles
+    
+        articles = sorted(articles.items(), key=lambda x: x[1])
+        articles = dict(articles)
+
+        data = {
+            "article_labels": articles.keys(),
+            "article_data": articles.values(),
+        }
+    
+        return Response(data)
